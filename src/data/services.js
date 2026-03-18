ceed07652d59eda305556d5cf7474137625af5ee
BlackSpiderTech <120741091+BlackSpiderTech@users.noreply.github.com>
2026-03-18 01:05:17 -0400

    Fixed service images imports

--- src/data/services.js
+++ src/data/services.js
@@ -1,23 +1,27 @@
+import residentialProjectsImg from "../assets/images/residentialprojects.webp";
+import conceptDevelopmentImg from "../assets/images/conceptDevelopment.jpg";
+import furnitureFinishesImg from "../assets/images/furniturefinishes.jpg";
+
 export const services = [
   {
     id: 1,
     title: "Residential & Commercial Interiors",
     description:
-      "We help make spaces functional, safe adn beautiful by determining space requirements and selecting decorative items, such as colors, lighting and materials.",
-    image: "/src/assets/images/residentialprojects.webp",
+      "We help make spaces functional, safe and beautiful by determining space requirements and selecting decorative items, such as colors, lighting and materials.",
+    image: residentialProjectsImg,
   },
   {
     id: 2,
     title: "Concept Development",
     description:
-      "With the use of techincal drawings and putting together mood boards with key images that best convey the essence of the brand, style and overall desired look",
-    image: "/src/assets/images/conceptDevelopment.jpg",
+      "With the use of technical drawings and putting together mood boards with key images that best convey the essence of the brand, style and overall desired look.",
+    image: conceptDevelopmentImg,
   },
   {
     id: 3,
-    title: "Furniture, Finishes & Accessory selections",
+    title: "Furniture, Finishes & Accessory Selections",
     description:
-      "We use careful selection and arrangement of accessories to transform an empty room or space into a finished space with a personality all of it's own",
-    image: "/src/assets/images/furniturefinishes.jpg",
+      "We use careful selection and arrangement of accessories to transform an empty room or space into a finished space with a personality all of its own.",
+    image: furnitureFinishesImg,
   },
 ];
